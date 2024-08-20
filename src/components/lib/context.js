09e1f55f5508d5d224155bfbfe5d90219548e948
Tim Wheelock <tlock@armyofbees.com>
2024-08-20 14:48:02 +0900

    Add cursor loading state during initialization.
    The initialization function in this library blocks the main thread with a synchronous loop over all possible months. This commit doesn't fix that, it just shows a loading spinner instead.

--- src/components/lib/context.js
+++ src/components/lib/context.js
@@ -7,7 +7,27 @@ import { ensureFutureMonth } from './date-manipulation.js'
 
 const contextKey = {}
 
+function setLoadingCursor () {
+  console.log('setting up loading cursor')
+  const loadingStyle = document.createElement('style')
+
+  loadingStyle.id = 'loading-style'
+
+  loadingStyle.innerHTML = '* { cursor: wait !important; }'
+
+  document.head.appendChild(loadingStyle)
+}
+
+function clearLoadingCursor () {
+  const loadingStyle = document.getElementById('loading-style')
+  setTimeout(() => {
+    console.log('removing loading cursor')
+    loadingStyle.remove()
+  }, 0)
+}
+
 function setup (given, config) {
+  setLoadingCursor()
   const today = dayjs().startOf('day')
 
   const { isDateChosen, chosen: [ preSelectedStart, preSelectedEnd ] } = sanitizeInitialValue(given, config)
@@ -21,7 +41,7 @@ function setup (given, config) {
   ).startOf('month')
   const rightDate = config.isRangePicker ? ensureFutureMonth(leftDate, preSelectedEnd).startOf('month') : null
 
-  return {
+  const setupObj = {
     months: getMonths(config),
     component,
     today,
@@ -41,6 +61,10 @@ function setup (given, config) {
     },
     isSelectingFirstDate: writable(true)
   }
+
+  clearLoadingCursor()
+
+  return setupObj
 }
 
 export {
